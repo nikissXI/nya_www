a488a90b2facf261f24b2e049e0a71595c969c62
nikissXI <1299577815@qq.com>
2024-10-05 19:07:35 +0800

    feat: 更新apk

--- public/apks/给ios用的联机工具代码.py
+++ public/apks/给ios用的联机工具代码.py
@@ -6,25 +6,10 @@ def start_udp_listener():
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     except Exception as e:
         exit(f"绑定失败: {e}")
-    print("等待数据包中...")
-    have_notice = False
-    packet_count = 0
-    while True:
-        try:
-            # 接收数据包
-            data, addr = sock.recvfrom(1024)
-            source_address = addr[0]
-            if source_address != "172.18.255.255":
-                if not have_notice:
-                    print("正在持续转发数据包\n加入成功后可关闭\n如需重新加入请重新运行")
-                    have_notice = True
-                if packet_count < 15:
-                    packet_count += 1
-                    continue
-                else:
-                    sock.sendto(data, ("172.18.255.255", 50124))
-                    packet_count = 0
-        except Exception as e:
-            print(f"接收出错: {e}")
-if __name__ == "__main__":
-    start_udp_listener()
+    print("游戏创建多人游戏后再切回来")
+    data, addr = sock.recvfrom(1024)
+    source_port = addr[1]  # 获取源端口
+    print("转发数据包成功，逃脱者QQ群内出现机器人的开房提示后请在1分钟内回到游戏里重新打开多人游戏模式")
+    for _ in range(3):
+        sock.sendto(data, ("172.18.255.255", source_port))
+start_udp_listener()
