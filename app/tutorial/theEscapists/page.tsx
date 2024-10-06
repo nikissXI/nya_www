c380ff0b0fc2b0de5ef9199e1254d367358a98ed
nikissXI <1299577815@qq.com>
2024-10-06 11:28:33 +0800

    feat: 变更逃脱者联机教程

--- app/tutorial/theEscapists/page.tsx
+++ app/tutorial/theEscapists/page.tsx
@@ -1,14 +1,69 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { VStack, Text } from "@chakra-ui/react";
+import {
+  Heading,
+  VStack,
+  Text,
+  Box,
+  Input,
+  Flex,
+  Center,
+} from "@chakra-ui/react";
 import { Button } from "@/components/universal/button";
+import { useState } from "react";
+import { useUserStateStore } from "@/store/user-state";
+import { useDisclosureStore } from "@/store/disclosure";
+import { openToast } from "@/components/universal/toast";
+import { getAuthToken } from "@/store/authKey";
+import { isInteger } from "@/utils/strings";
 
 export default function AndroidPage0() {
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
   const router = useRouter();
+  const { logined } = useUserStateStore();
+  const [inputNum, setInputNum] = useState("");
+  const [showText, setShowText] = useState("");
+
+  const { onToggle: loginToggle } = useDisclosureStore((state) => {
+    return state.modifyLoginDisclosure;
+  });
+
+  const createHelper = async (hosterWgnum: string) => {
+    if (!hosterWgnum) {
+      return;
+    }
+
+    if (!isInteger(hosterWgnum)) {
+      openToast({ content: `请输入正确的编号` });
+      return;
+    }
+
+    const resp = await fetch(
+      `${apiUrl}/escapistsHelper?hosterWgnum=${hosterWgnum}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${getAuthToken()}`,
+        },
+      }
+    );
+
+    if (resp.ok) {
+      const data = await resp.json();
+      if (data.code === 0) {
+        setShowText(data.msg);
+      } else {
+        setShowText(data.msg);
+      }
+    } else {
+      setShowText("服务异常，若刷新仍旧异常，请联系服主处理");
+    }
+  };
 
   return (
-    <VStack spacing={6}>
-      <Text align="center">
+    <Center>
+      <VStack spacing={3} maxW={{ base: "90vw", md: "400px" }}>
+        {/* <Text align="center">
         由于该游戏的联机机制特殊
         <br />
         需要额外工具辅助才能建房
@@ -24,19 +79,70 @@ export default function AndroidPage0() {
         支持安卓和苹果联机
         <br />
         不建议iPhone做房主，其他人很难加入
-      </Text>
+      </Text> */}
 
-      <Button
-        size="sm"
-        bgColor="#c1447d"
-        onClick={() => {
-          window.open("https://b23.tv/2dcjPHv", "_blank");
-        }}
-      >
-        点击查看视频教程
-      </Button>
+        <Button
+          size="sm"
+          bgColor="#c1447d"
+          onClick={() => {
+            window.open("https://b23.tv/2dcjPHv", "_blank");
+          }}
+        >
+          点击查看视频教程
+        </Button>
 
-      <Button
+        <Text>由于这游戏的联机机制很傻逼，联机需要辅助手段，喵服这边已经给你做了最简化操作</Text>
+        <Text>房主那边连上喵服后进游戏创建多人游戏就行，加入者那边需要用下面两种方式中的一种才能搜索到房间</Text>
+        <Text>两种方式本质上是一样的，只不过一种在网页操作，一种在QQ里操作</Text>
+        <Text>每次搜索房间都得操作一遍，你不要觉得麻烦，这是全网最简单的操作了，懒的搞就换个游戏吧</Text>
+
+        <Heading size="md" mt={2}>
+          方式一（网页操作）
+        </Heading>
+        <Text>
+          在下方填写创建逃脱者多人游戏的玩家编号（不是喵服联机房间的房主编号，别搞混了），然后点提交，会有对应的回显
+        </Text>
+
+        <Flex>
+          {logined ? (
+            <Box>
+              <Flex>
+                <Input
+                  w="130px"
+                  type="number"
+                  value={inputNum}
+                  onChange={(e) => {
+                    setInputNum(e.target.value);
+                  }}
+                  placeholder="要加入的编号"
+                />
+
+                <Button
+                  onClick={() => {
+                    createHelper(inputNum);
+                  }}
+                >
+                  提交
+                </Button>
+              </Flex>
+
+              <Text color="#ffd648">{showText}</Text>
+            </Box>
+          ) : (
+            <Button bgColor="#1d984b" size="sm" onClick={loginToggle}>
+              请登录后再操作，点击登录
+            </Button>
+          )}
+        </Flex>
+
+        <Heading size="md" mt={2}>
+          方式二（QQ群操作）
+        </Heading>
+        <Text>
+          加入QQ群961793250，在群里发命令“搜索”，如要搜索2号创建的房间，就发“搜索2”，机器人会给对应提示
+        </Text>
+
+        {/* <Button
         p={7}
         bgColor="#148f14"
         fontSize="30px"
@@ -56,18 +162,19 @@ export default function AndroidPage0() {
         }}
       >
         苹果
-      </Button>
+      </Button> */}
 
-      <Button
-        size="lg"
-        variant="link"
-        bgColor="transparent"
-        onClick={() => {
-          router.push("/room");
-        }}
-      >
-        返回
-      </Button>
-    </VStack>
+        <Button
+          size="lg"
+          variant="link"
+          bgColor="transparent"
+          onClick={() => {
+            router.push("/room");
+          }}
+        >
+          返回
+        </Button>
+      </VStack>
+    </Center>
   );
 }
--- components/tutorial/nya/pc/Page2.tsx
+++ components/tutorial/nya/pc/Page2.tsx
@@ -63,6 +63,7 @@ export function Page() {
           <ListItem>
             在左下角点新建隧道，然后选下载的conf文件，完成导入
           </ListItem>
+
           <ListItem>点连接按钮，连上喵服</ListItem>
         </List>
 
