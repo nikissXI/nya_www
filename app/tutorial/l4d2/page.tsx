89b3bef9f2e7c27a63b009ed8c572e11bbb6bd1f
nikissXI <1299577815@qq.com>
2024-10-29 12:10:54 +0800

    docs: 更新求生之路2教程

--- app/tutorial/l4d2/page.tsx
+++ app/tutorial/l4d2/page.tsx
@@ -17,6 +17,20 @@ export default function Page() {
         mx="5vw"
         maxW="460px"
       >
+        <Button
+          bgColor="#c1447d"
+          onClick={() => {
+            window.open(
+              "https://www.bilibili.com/video/BV1xHS4Y7Ejz/",
+              "_blank"
+            );
+          }}
+        >
+          点击查看视频教程
+        </Button>
+
+        <Divider my={5} />
+
         <Text my={3}>玩家都处于一个联机房间后，进游戏点“与好友一起玩游戏”</Text>
         <Image src="/images/l4d2_1.jpg" alt="l4d2_1" />
 
@@ -33,8 +47,18 @@ export default function Page() {
         <Divider my={5} />
 
         <Text my={3}>
-          加入的玩家按波浪键“~”调出控制台，使用命令“connect 房主IP”即可加入
+          加入的玩家按波浪键“~”调出控制台，使用命令“connect 主机IP”即可加入
         </Text>
+
+        <Divider my={5} />
+
+        <Text my={3}>建议到创意工坊把这个mod打上，能避免兼容性问题导致无法加入</Text>
+        <Image
+          src="/images/l4d2_关闭一致性检查.jpg"
+          alt="l4d2_关闭一致性检查"
+        />
+
+        <Divider my={5} />
 
         <Button
           bgColor="#b23333"
