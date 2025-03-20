d2ae4bde96edea4de60c4e194bc1553a18ef7efa
nikissXI <1299577815@qq.com>
2025-03-20 23:38:48 +0800

    docs: 更新饥荒教程

--- app/docs/doNotStarve/page.tsx
+++ app/docs/doNotStarve/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Flex, Center, Divider, Text } from "@chakra-ui/react";
+import { Flex, Center, Divider, Text, Image } from "@chakra-ui/react";
 import { Button } from "@/components/universal/button";
 import { useRouter } from "next/navigation";
 
@@ -16,10 +16,11 @@ export default function Page() {
         mb={5}
         mx="5vw"
       >
+        <Image src="/images/doNotStarve/lan.png" alt="lan" />
         <Text my={3}>
-          玩家都处于一个联机房间后，在局域网联机模式填主机IP加入就行。
+          玩家都处于一个联机房间后，主机创建多人游戏，客机进浏览游戏，右上角的连接选LAN，然后刷新房间列表即可。
           <br />
-          由于服主没买饥荒，详细教程待补充，或者自己搜搜网上的教程，把教程里的IP换成喵服的IP就行。
+          如果搜不到，主机关闭系统防火墙后再试。
         </Text>
 
         <Divider my={5} />
