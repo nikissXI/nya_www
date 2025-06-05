aa8abf687b1002fde6601aa6bb29e2a476960dc9
nikissXI <1299577815@qq.com>
2025-06-05 11:43:04 +0800

    feat: 赞助表实时更新

--- app/sponsor/page.tsx
+++ app/sponsor/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React, { useEffect, useState } from "react";
 import {
   Box,
   Text,
@@ -14,6 +15,7 @@ import {
   Tbody,
   Td,
 } from "@chakra-ui/react";
+import { openToast } from "@/components/universal/toast";
 
 interface SponsorItem {
   uid: number;
@@ -21,494 +23,37 @@ interface SponsorItem {
   sponsorship: number;
 }
 
-const sponsorList: SponsorItem[] = [
-  {
-    uid: 7080,
-    username: "R32",
-    sponsorship: 1688,
-  },
-  {
-    uid: 446,
-    username: "浅梦",
-    sponsorship: 444,
-  },
-  {
-    uid: 650,
-    username: "白橘子抱枕",
-    sponsorship: 360,
-  },
-  {
-    uid: 668,
-    username: "独酌对月心思故",
-    sponsorship: 333,
-  },
-  {
-    uid: 9,
-    username: "帕瓦",
-    sponsorship: 328,
-  },
-  {
-    uid: 1338,
-    username: "说多都是泪",
-    sponsorship: 321,
-  },
-  {
-    uid: 7055,
-    username: "慕逢春",
-    sponsorship: 302,
-  },
-  {
-    uid: 7084,
-    username: "你笑",
-    sponsorship: 300,
-  },
-  {
-    uid: 4,
-    username: "伪善",
-    sponsorship: 274,
-  },
-  {
-    uid: 7064,
-    username: "空谷幽兰",
-    sponsorship: 240,
-  },
-  {
-    uid: 7050,
-    username: "白眼",
-    sponsorship: 233,
-  },
-  {
-    uid: 11,
-    username: "大梦谁先觉",
-    sponsorship: 210,
-  },
-  {
-    uid: 174,
-    username: "门屠",
-    sponsorship: 206,
-  },
-  {
-    uid: 1336,
-    username: "柜人多望事",
-    sponsorship: 206,
-  },
-  {
-    uid: 6984,
-    username: "瑶某人",
-    sponsorship: 200,
-  },
-  {
-    uid: 7065,
-    username: "用户1499",
-    sponsorship: 200,
-  },
-  {
-    uid: 1337,
-    username: "水中巴黎",
-    sponsorship: 200,
-  },
-  {
-    uid: 406,
-    username: "落羽之地",
-    sponsorship: 200,
-  },
-  {
-    uid: 6948,
-    username: "用户1832",
-    sponsorship: 200,
-  },
-  {
-    uid: 1335,
-    username: "伊邪那岐小神",
-    sponsorship: 200,
-  },
-  {
-    uid: 175,
-    username: "咕言咕语",
-    sponsorship: 200,
-  },
-  {
-    uid: 557,
-    username: "Chidori子不语",
-    sponsorship: 200,
-  },
-  {
-    uid: 779,
-    username: "云歌",
-    sponsorship: 200,
-  },
-  {
-    uid: 7026,
-    username: "用户9650",
-    sponsorship: 200,
-  },
-  {
-    uid: 6977,
-    username: "zzZZZZ",
-    sponsorship: 200,
-  },
-  {
-    uid: 113,
-    username: "胡子大叔",
-    sponsorship: 200,
-  },
-  {
-    uid: 164,
-    username: "王有胜",
-    sponsorship: 162,
-  },
-  {
-    uid: 101,
-    username: "夢想楽土",
-    sponsorship: 150,
-  },
-  {
-    uid: 7162,
-    username: "用户2789",
-    sponsorship: 150,
-  },
-  {
-    uid: 38,
-    username: "阿宝",
-    sponsorship: 130,
-  },
-  {
-    uid: 62,
-    username: "Hannibal",
-    sponsorship: 130,
-  },
-  {
-    uid: 6943,
-    username: "失约",
-    sponsorship: 130,
-  },
-  {
-    uid: 7110,
-    username: "2124637990",
-    sponsorship: 130,
-  },
-  {
-    uid: 7146,
-    username: "雏田",
-    sponsorship: 124,
-  },
-  {
-    uid: 6957,
-    username: "正经人",
-    sponsorship: 120,
-  },
-  {
-    uid: 116,
-    username: "百分百纯度",
-    sponsorship: 114,
-  },
-  {
-    uid: 185,
-    username: "猫猫",
-    sponsorship: 105,
-  },
-  {
-    uid: 2324,
-    username: "忧蓝的月亮",
-    sponsorship: 104,
-  },
-  {
-    uid: 112,
-    username: "ミ隱世く",
-    sponsorship: 100,
-  },
-  {
-    uid: 7173,
-    username: "六花等着你",
-    sponsorship: 100,
-  },
-  {
-    uid: 7108,
-    username: "红袖",
-    sponsorship: 100,
-  },
-  {
-    uid: 7002,
-    username: "AIs",
-    sponsorship: 100,
-  },
-  {
-    uid: 7016,
-    username: "Shooting",
-    sponsorship: 100,
-  },
-  {
-    uid: 330,
-    username: "铱盐盯帧",
-    sponsorship: 100,
-  },
-  {
-    uid: 7061,
-    username: "一个句号",
-    sponsorship: 100,
-  },
-  {
-    uid: 7076,
-    username: "雾雨城久梦",
-    sponsorship: 100,
-  },
-  {
-    uid: 7085,
-    username: "用户1812",
-    sponsorship: 100,
-  },
-  {
-    uid: 7078,
-    username: "清风明月几时有",
-    sponsorship: 100,
-  },
-  {
-    uid: 417,
-    username: "优莱卡",
-    sponsorship: 100,
-  },
-  {
-    uid: 440,
-    username: "元海",
-    sponsorship: 100,
-  },
-  {
-    uid: 502,
-    username: "坏丫头冬冬",
-    sponsorship: 100,
-  },
-  {
-    uid: 6,
-    username: "AOUI",
-    sponsorship: 100,
-  },
-  {
-    uid: 10,
-    username: "十六",
-    sponsorship: 100,
-  },
-  {
-    uid: 7,
-    username: "用户2954",
-    sponsorship: 100,
-  },
-  {
-    uid: 26,
-    username: "随笔",
-    sponsorship: 100,
-  },
-  {
-    uid: 27,
-    username: "叶枼耶耶椰",
-    sponsorship: 100,
-  },
-  {
-    uid: 28,
-    username: "用户3061",
-    sponsorship: 100,
-  },
-  {
-    uid: 45,
-    username: "我很好谢谢你呢",
-    sponsorship: 100,
-  },
-  {
-    uid: 46,
-    username: "暗の鬼炎",
-    sponsorship: 100,
-  },
-  {
-    uid: 49,
-    username: "王德发",
-    sponsorship: 100,
-  },
-  {
-    uid: 66,
-    username: "格拉斯贝恩",
-    sponsorship: 100,
-  },
-  {
-    uid: 68,
-    username: "文森六世",
-    sponsorship: 100,
-  },
-  {
-    uid: 82,
-    username: "茶綠喵_绿茶",
-    sponsorship: 100,
-  },
-  {
-    uid: 90,
-    username: "kemomimi酱",
-    sponsorship: 100,
-  },
-  {
-    uid: 142,
-    username: "蚊子",
-    sponsorship: 100,
-  },
-  {
-    uid: 125,
-    username: "阿伟",
-    sponsorship: 100,
-  },
-  {
-    uid: 180,
-    username: "脆脆鲨",
-    sponsorship: 100,
-  },
-  {
-    uid: 6990,
-    username: "DS啊",
-    sponsorship: 100,
-  },
-  {
-    uid: 6966,
-    username: "XQ",
-    sponsorship: 100,
-  },
-  {
-    uid: 6994,
-    username: "用户6090",
-    sponsorship: 100,
-  },
-  {
-    uid: 7115,
-    username: "Mean机器",
-    sponsorship: 100,
-  },
-  {
-    uid: 7117,
-    username: "圣愿之祭",
-    sponsorship: 100,
-  },
-  {
-    uid: 7147,
-    username: "松山柳柏",
-    sponsorship: 100,
-  },
-  {
-    uid: 7193,
-    username: "well",
-    sponsorship: 100,
-  },
-  {
-    uid: 71,
-    username: "萧鋈",
-    sponsorship: 69,
-  },
-  {
-    uid: 7189,
-    username: "Cator",
-    sponsorship: 66,
-  },
-  {
-    uid: 7025,
-    username: "慕雨 丶无痕",
-    sponsorship: 66,
-  },
-  {
-    uid: 7120,
-    username: "hubris",
-    sponsorship: 63,
-  },
-  {
-    uid: 5,
-    username: "清吾",
-    sponsorship: 60,
-  },
-  {
-    uid: 7112,
-    username: "元戎还望",
-    sponsorship: 60,
-  },
-  {
-    uid: 6980,
-    username: "微笑",
-    sponsorship: 60,
-  },
-  {
-    uid: 7144,
-    username: "白色月牙",
-    sponsorship: 60,
-  },
-  {
-    uid: 122,
-    username: "青空",
-    sponsorship: 50,
-  },
-  {
-    uid: 169,
-    username: "无语Y",
-    sponsorship: 50,
-  },
-  {
-    uid: 85,
-    username: "小肥羊",
-    sponsorship: 50,
-  },
-  {
-    uid: 7122,
-    username: "八嘎呀路",
-    sponsorship: 50,
-  },
-  {
-    uid: 8,
-    username: "忆梦",
-    sponsorship: 50,
-  },
-  {
-    uid: 15306,
-    username: "阿龙龙",
-    sponsorship: 50,
-  },
-  {
-    uid: 15771,
-    username: "wowaka",
-    sponsorship: 50,
-  },
-  {
-    uid: 7054,
-    username: "用户1339",
-    sponsorship: 50,
-  },
-  {
-    uid: 335,
-    username: "夜",
-    sponsorship: 50,
-  },
-  {
-    uid: 7149,
-    username: "Rainbow",
-    sponsorship: 50,
-  },
-  {
-    uid: 7137,
-    username: "小幽",
-    sponsorship: 50,
-  },
-  {
-    uid: 6962,
-    username: "MuMu",
-    sponsorship: 50,
-  },
-  {
-    uid: 7181,
-    username: "拓尔米基始",
-    sponsorship: 50,
-  },
-  {
-    uid: 7074,
-    username: "haha",
-    sponsorship: 50,
-  },
-];
-
-// const maskNumber = (number: string): string => {
-//   return number.replace(/.(?=.{4})/g, "*");
-// };
-
 const Page = () => {
+  const [sponsorList, setSponsorList] = useState<SponsorItem[]>([]);
+
+  useEffect(() => {
+    async function fetchSponsors() {
+      try {
+        const res = await fetch("https://nyaapi.nikiss.top/sponsorList");
+        if (!res.ok) {
+          throw new Error(`请求失败，状态码：${res.status}`);
+        }
+        const data = await res.json();
+        if (data.code === 0 && Array.isArray(data.data)) {
+          // 过滤出赞助金额不低于50的
+          const filtered = data.data.filter(
+            (item: SponsorItem) => item.sponsorship >= 50
+          );
+          setSponsorList(filtered);
+        } else {
+          openToast({ content: `响应出错 ${data.msg}`, status: "error" });
+        }
+      } catch (err) {
+        openToast({
+          content: (err as Error).message || "请求发生错误",
+          status: "error",
+        });
+      }
+    }
+
+    fetchSponsors();
+  }, []);
+
   return (
     <Box
       display="flex"
@@ -552,7 +97,8 @@ const Page = () => {
         </TableContainer>
 
         <Text textAlign="left">
-          喵服由服主一人纯公益运营，赞助将帮助喵服持续提供联机服务。服主QQ 1299577815
+          喵服由服主一人纯公益运营，赞助将帮助喵服持续提供联机服务。服主QQ
+          1299577815
           <br />
           如果在付款时备注您的喵服账号UID（不是编号别搞错了，在我的信息页面查看），赞助金额可被系统记录并获得赞助者铭牌。
         </Text>
