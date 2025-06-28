c186c7f642a1f227d129c0e40cf403f6447f5609
nikissXI <1299577815@qq.com>
2025-06-28 22:58:04 +0800

    feat: 图片验证码改为直链

--- utils/GetCaptcha.ts
+++ utils/GetCaptcha.ts
@@ -8,15 +8,17 @@ const useCaptcha = () => {
   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
   const { uuid } = useUserStateStore();
+
   const fetchCaptcha = useCallback(async () => {
-    const resp = await fetch(`${apiUrl}/getCaptcha?uuid=${uuid}`);
-    if (resp.ok) {
-      const blob = await resp.blob(); // 获取 Blob 对象
-      return URL.createObjectURL(blob); // 返回图片地址
-    } else {
-      openToast({ content: "验证码加载失败，点击重试", status: "error" });
-      throw new Error("拉取图片验证码失败"); // 错误处理
-    }
+    return `${apiUrl}/getCaptcha?uuid=${uuid}&t=${Date.now()}`;
+    // const resp = await fetch(`${apiUrl}/getCaptcha?uuid=${uuid}`);
+    // if (resp.ok) {
+    //   const blob = await resp.blob(); // 获取 Blob 对象
+    //   return URL.createObjectURL(blob); // 返回图片地址
+    // } else {
+    //   openToast({ content: "验证码加载失败，点击重试", status: "error" });
+    //   throw new Error("拉取图片验证码失败"); // 错误处理
+    // }
   }, [uuid, apiUrl]);
 
   return { fetchCaptcha };
