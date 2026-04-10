188cfa8cc1a1c4131f42ef77c0ae75b8270823f9
nikissXI <1299577815@qq.com>
2026-04-10 22:55:37 +0800

    docs: 更新维护日期公告为4月11日

--- components/docs/Announcement.tsx
+++ components/docs/Announcement.tsx
@@ -41,7 +41,7 @@ export const AnnouncementsModal = ({}) => {
     <Box textAlign="center">
       <Box mx={5} mb={3} fontWeight="bold">
         <Text color="#ffca3d" size="sm">
-          4月10日上午维护，合并部分节点
+          4月11日上午维护，合并部分节点
         </Text>
 
         <Flex justify="center" align="center" wrap="wrap">
