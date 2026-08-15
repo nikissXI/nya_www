68f950d8d8f3be5ac15310ece32213cd17b44074
nikissXI <1299577815@qq.com>
2026-08-15 12:56:27 +0800

    debug

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 新机部署先clone
+# git clone git@github.com:nikissXI/nya_www.git
+
+echo "pulling project..."
+set -e
+git pull --rebase
+
+echo "Installing dependencies and building the project..."
+pnpm install
+pnpm build
+pnpm start
