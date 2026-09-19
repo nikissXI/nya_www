2cd384b6d5e99a528c6e4058ea8b6fb2dcb4e02e
nikissXI <1299577815@qq.com>
2026-09-19 20:35:58 +0800

    docs: ark联机教程更新

--- app/docs/ark/page.tsx
+++ app/docs/ark/page.tsx
@@ -24,13 +24,13 @@ export default function Page() {
         "2-3人联机时，建议选择带宽不低于0.5M的联机节点，人更多时选择更大带宽节点",
       ]}
     >
-      <Text >这里只有手游的联机教程，如果是端游请找服主</Text>
+      <Text>这里只有手游的联机教程，如果是端游请找服主</Text>
 
       <Divider my={5} />
 
       <Text textAlign="center"></Text>
 
-      <Text >喵服方舟手游交流Q群 1106534252</Text>
+      <Text>喵服方舟手游交流Q群 1106534252</Text>
       <Text fontSize="sm" color="gray.300" mt={1}>
         联机有问题或找搭子请加群
       </Text>
@@ -113,6 +113,35 @@ export default function Page() {
         <Text>
           <Icon as={MdTipsAndUpdates} mr={1} />
           如果加入游戏后闪退，检查双方游戏版本是否一致，如果是一致，主机重开游戏试试，还不行就进群问问
+        </Text>
+      </VStack>
+
+      <Divider my={5} />
+
+      <Heading size="md" mb={3}>
+        修改联机游戏距离
+      </Heading>
+
+      <VStack align="stretch" spacing={1}>
+        <Text>
+          <Icon as={MdTipsAndUpdates} mr={1} />
+          找到游戏的配置文件
+          GameUserSettings.ini，找到下方的这个设置并将数值改为40.00000
+          <Code
+            display="block"
+            whiteSpace="pre-wrap"
+            p={1}
+            borderRadius="md"
+            bg="gray.900"
+            color="cyan.200"
+            fontSize="sm"
+            fontFamily="mono"
+          >
+            ListenServerTetherDistanceMultiplier=40.00000
+          </Code>
+        </Text>
+        <Text fontSize="sm" color="gray.300">
+          只需要主机修改，40为建议值，太大也不行
         </Text>
       </VStack>
 
